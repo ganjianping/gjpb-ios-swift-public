40a34c81b131d2b9db8180f1c67b197838452a79
ganjianping <ganjianping1984@gmail.com>
2026-02-28 23:25:05 +0800

    Update code

---
+++ GJPBTests/GJPBTests.swift
@@ -0,0 +1,17 @@
+//
+//  GJPBTests.swift
+//  GJPBTests
+//
+//  Created by Gan Jianping on 28/2/26.
+//
+
+import Testing
+@testable import GJPB
+
+struct GJPBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
